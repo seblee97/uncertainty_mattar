398d630d96bcab635a5c34800e87f9b6fa95d11b
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2025-01-13 11:33:15 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='uncmattar',
+    version='0.1.0',
+    description='Models of PER encoporating uncertainty metrics',
+    author='Sebastian Lee',
+    author_email='sebastianlee.1997@yahoo.co.uk',
+    packages=find_packages(), 
+    install_requires=[],
+)
