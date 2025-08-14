5831e528a1839cfcbc46d26c0d22a9a48ac3836f
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2025-08-14 11:01:54 -0400

    run modes

--- unc_mattar/runners/base_runner.py
+++ unc_mattar/runners/base_runner.py
@@ -1,22 +1,29 @@
 from key_door import key_door_env
 from key_door import visualisation_env
 
+from run_modes import base_runner
+
 import os
+
+from typing import List
 
 from unc_mattar import constants
 from unc_mattar.agents import q_learner, dyna_learner
 
 import abc
 import numpy as np
+import pandas as pd
 
 
-class BaseRunner(abc.ABC):
+class BaseRunner(base_runner.BaseRunner, abc.ABC):
 
     def __init__(
         self,
         config,
         unique_id: str = "",
     ):
+
+        base_runner.BaseRunner.__init__(self, config=config, unique_id=unique_id)
 
         self._learning_rate = config.learning_rate
         self._beta = config.beta
@@ -36,7 +43,10 @@ class BaseRunner(abc.ABC):
                 f"Initialisation strategy {config.initialisation_strategy} not recognised."
             )
 
-        self._checkpoint_path = config.checkpoint_path
+        self._checkpoint_frequency = config.checkpoint_frequency
+        self._data_columns = self._setup_data_columns()
+        self._log_columns = self._get_data_columns()
+
         self._heatmap_path = os.path.join(self._checkpoint_path, constants.HEATMAPS)
         os.makedirs(self._heatmap_path, exist_ok=True)
         self._video_path = os.path.join(self._checkpoint_path, constants.VIDEOS)
@@ -62,11 +72,13 @@ class BaseRunner(abc.ABC):
         self._test_env = visualisation_env.VisualisationEnv(_test_env)
 
     def train(self):
-        train_episode_returns = []
-        train_episode_lengths = []
+        # train_episode_returns = []
+        # train_episode_lengths = []
 
-        test_episode_returns = []
-        test_episode_lengths = []
+        # test_episode_returns = []
+        # test_episode_lengths = []
+
+        self._data_index = 0
 
         for i in range(self._num_episodes):
 
@@ -96,18 +108,62 @@ class BaseRunner(abc.ABC):
             train_episode_return, train_episode_length = self._train_episode()
             test_episode_return, test_episode_length = self._test_episode()
 
-            train_episode_returns.append(train_episode_return)
-            train_episode_lengths.append(train_episode_length)
+            self._data_columns[constants.TRAIN_EPISODE_RETURN][
+                self._data_index
+            ] = train_episode_return
+            self._data_columns[constants.TRAIN_EPISODE_LENGTH][
+                self._data_index
+            ] = train_episode_length
+            self._data_columns[constants.TEST_EPISODE_RETURN][
+                self._data_index
+            ] = test_episode_return
+            self._data_columns[constants.TEST_EPISODE_LENGTH][
+                self._data_index
+            ] = test_episode_length
 
-            test_episode_returns.append(test_episode_return)
-            test_episode_lengths.append(test_episode_length)
+            self._data_index += 1
 
-        return (
-            train_episode_returns,
-            train_episode_lengths,
-            test_episode_returns,
-            test_episode_lengths,
-        )
+            if i % self._checkpoint_frequency == 0:
+                self._checkpoint_data()
+
+        #     train_episode_returns.append(train_episode_return)
+        #     train_episode_lengths.append(train_episode_length)
+
+        #     test_episode_returns.append(test_episode_return)
+        #     test_episode_lengths.append(test_episode_length)
+
+        # return (
+        #     train_episode_returns,
+        #     train_episode_lengths,
+        #     test_episode_returns,
+        #     test_episode_lengths,
+        # )
+
+    def _get_data_columns(self):
+        """Output data columns to be logged by runner."""
+        columns = [
+            constants.TRAIN_EPISODE_RETURN,
+            constants.TRAIN_EPISODE_LENGTH,
+            constants.TEST_EPISODE_RETURN,
+            constants.TEST_EPISODE_LENGTH,
+        ]
+        return columns
+
+    def _setup_data_columns(self):
+        data_columns = {}
+        for key in self._get_data_columns():
+            arr = np.empty(self._checkpoint_frequency)
+            arr[:] = np.nan
+            data_columns[key] = arr
+
+        return data_columns
+
+    def _checkpoint_data(self):
+        log_dict = {k: self._data_columns[k] for k in self._log_columns}
+        self._data_logger.logger_data = pd.DataFrame.from_dict(log_dict)
+        self._data_logger.checkpoint()
+        self._data_columns = self._setup_data_columns()
+        self._data_index = 0
 
     @abc.abstractmethod
     def _train_episode(self):
