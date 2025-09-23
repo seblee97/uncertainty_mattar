846cde5397c4bb4cdb4d1b17c0c3dac90cc81123
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2025-09-23 11:14:03 -0400

    fix on buffer size

--- unc_mattar/utils.py
+++ unc_mattar/utils.py
@@ -33,23 +33,41 @@ class ReplayBuffer:
         self._predecessors_inv: Dict[int, int] = {}
 
         self._index = 0
+        self._current_size = 0  # Track the actual size of the buffer
 
-    def add(self, state, action, reward, next_state, active):
-        if self._index == self._max_size:
-            # existing index will be overwritten, so remove from predecessors
-            old_next_state = self._predecessors_inv[self._index]
-            self._predecessors[old_next_state].remove(self._index)
+    def add(
+        self,
+        state: int,
+        action: int,
+        reward: float,
+        next_state: int,
+        active: bool,
+    ) -> None:
+        overwritten_index = self._index  # Track the index being overwritten
+
+        if overwritten_index in self._predecessors_inv:
+            # Remove the overwritten index from predecessors
+            old_next_state = self._predecessors_inv[overwritten_index]
+            self._predecessors[old_next_state].remove(overwritten_index)
+            if not self._predecessors[old_next_state]:  # Clean up empty lists
+                del self._predecessors[old_next_state]
+            del self._predecessors_inv[overwritten_index]
 
         self._states[self._index] = state
         self._actions[self._index] = action
         self._rewards[self._index] = reward
         self._next_states[self._index] = next_state
         self._actives[self._index] = active
+
+        # Update predecessors
         if next_state not in self._predecessors:
             self._predecessors[next_state] = []
         self._predecessors[next_state].append(self._index)
         self._predecessors_inv[self._index] = next_state
+
+        # Update the index and size
         self._index = (self._index + 1) % self._max_size
+        self._current_size = min(self._current_size + 1, self._max_size)
 
     def get(self, index: int):
         return (
@@ -61,13 +79,14 @@ class ReplayBuffer:
         )
 
     def get_predecessors(self, state_id: int) -> List[int]:
-        if state_id in self._predecessors:
-            return self._predecessors[state_id]
-        else:
-            return []
+        if state_id not in self._predecessors:
+            print(f"Warning: State ID {state_id} has no predecessors.")
+        return self._predecessors.get(state_id, [])
 
     @property
     def buffer(self):
+        if len(self) == 0:
+            return None  # Handle empty buffer case
         size = len(self)
         return (
             self._states[:size],
@@ -78,4 +97,4 @@ class ReplayBuffer:
         )
 
     def __len__(self):
-        return min(self._index, self._max_size)
+        return self._current_size
