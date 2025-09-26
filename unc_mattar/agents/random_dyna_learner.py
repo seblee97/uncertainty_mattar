29d210abbfd021ee7d819bd1ce3928f1cb441ca9
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2025-09-26 17:45:34 -0400

    per state counts

--- unc_mattar/agents/random_dyna_learner.py
+++ unc_mattar/agents/random_dyna_learner.py
@@ -10,12 +10,19 @@ class RandomDynaLearner(base_dyna_learner.DynaLearner):
         super().__init__(*args, **kwargs)
 
     def plan(self, current_state):
+
         # Sample a random transition from the replay buffer
         transition_sample = self._replay_buffer.get_random()
         transition_sample = transition_sample + (self._planning_lr,)
 
         self._state_planning_counts[self._id_state_mapping[transition_sample[0]]] += 1
+        self._per_state_state_planning_counts[current_state][
+            self._id_state_mapping[transition_sample[0]]
+        ] += 1
         self._episode_state_planning_counts[
+            self._id_state_mapping[transition_sample[0]]
+        ] += 1
+        self._episode_per_state_state_planning_counts[current_state][
             self._id_state_mapping[transition_sample[0]]
         ] += 1
 
